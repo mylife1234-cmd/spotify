66a12b7c2ba529e60c644dee3a0299c3bb0b42f4
Duy <M.Duy2001@gmail.com>
2022-03-18 00:54:07 +0700

    Added the playlists section

--- lib/pages/library.dart
+++ lib/pages/library.dart
@@ -13,6 +13,45 @@ class LibraryPage extends StatefulWidget {
 class _LibraryPageState extends State<LibraryPage> {
   final filterOptions = ['Playlists', 'Artists', 'Albums'];
 
+  final playlists = [
+    {
+      'title': 'Liked Songs',
+      'subtitle': 'Playlist',
+      'cover': 'assets/images/favorite.png',
+      'type': 'playlist'
+    },
+    {
+      'title': 'Đen',
+      'subtitle': 'Artist',
+      'cover': 'assets/images/den-vau.jpeg',
+      'type': 'artist'
+    },
+    {
+      'title': 'Charlie Puth',
+      'subtitle': 'Artist',
+      'cover': 'assets/images/charlie-puth.jpeg',
+      'type': 'artist'
+    },
+    {
+      'title': 'Billie Eilish',
+      'subtitle': 'Artist',
+      'cover': 'assets/images/billie-eilish.jpeg',
+      'type': 'artist'
+    },
+    {
+      'title': 'Bích Phương',
+      'subtitle': 'Artist',
+      'cover': 'assets/images/bich-phuong.jpeg',
+      'type': 'artist'
+    },
+    {
+      'title': 'Maroon 5',
+      'subtitle': 'Artist',
+      'cover': 'assets/images/maroon5.jpeg',
+      'type': 'artist'
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -32,7 +71,33 @@ class _LibraryPageState extends State<LibraryPage> {
                         .toList(),
                   ),
                 ),
-                const ViewModeSection()
+                const Padding(
+                  padding: EdgeInsets.only(bottom: 10.0),
+                  child: ViewModeSection(),
+                ),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: playlists.length,
+                    itemBuilder: (context, index) {
+                      final cover = playlists[index]['cover'];
+
+                      return Padding(
+                        padding: const EdgeInsets.only(bottom: 3.0),
+                        child: ListTile(
+                          title: Text(playlists[index]['title']!),
+                          subtitle: Text(playlists[index]['subtitle']!),
+                          leading: playlists[index]['type']! == 'playlist'
+                              ? Image.asset(cover!)
+                              : CircleAvatar(
+                                  foregroundImage: AssetImage(cover!),
+                                  radius: 28
+                                ),
+                          contentPadding: EdgeInsets.zero,
+                        ),
+                      );
+                    },
+                  ),
+                )
               ],
             ),
           ),
