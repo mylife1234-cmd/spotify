d96bc02f8687f3694dfbcb697fa6e7d280fdb6fb
Lppt12345 <aibiettoi19@gmail.com>
2022-03-22 02:56:55 +0700

    Update album_view

---
+++ lib/components/album/shuffle_button.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ShuffleButton extends StatelessWidget {
+  const ShuffleButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 20,
+      height: 20,
+      decoration: const BoxDecoration(
+        shape: BoxShape.circle,
+        color: Color(0xff2a2a2a),
+      ),
+      child: const Icon(
+        CupertinoIcons.shuffle,
+        color: Colors.green,
+        size: 12,
+      ),
+    );
+  }
+}
