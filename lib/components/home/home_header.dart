e9eec4454ddfa561b9002cabbffd24e473feb166
Lppt12345 <aibiettoi19@gmail.com>
2022-03-22 03:06:58 +0700

    add home header

---
+++ lib/components/home/home_header.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomeHeader extends StatelessWidget {
+  const HomeHeader({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 24.0, bottom: 15.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const Text('Recently Played',
+              style: TextStyle(
+                  fontSize: 18, fontWeight: FontWeight.bold)),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: const [
+              Icon(Icons.notifications),
+              SizedBox(width: 15),
+              Icon(Icons.history),
+              SizedBox(width: 15),
+              Icon(Icons.settings),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
