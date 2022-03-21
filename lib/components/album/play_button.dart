445842ce279f94ca4475c4910f68ff3b84af06d2
Lppt12345 <aibiettoi19@gmail.com>
2022-03-22 02:59:50 +0700

    Add play button

---
+++ lib/components/album/play_button.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class PLayButton extends StatelessWidget {
+  const PLayButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 48,
+      height: 48,
+      alignment: Alignment.center,
+      decoration: const BoxDecoration(
+        shape: BoxShape.circle,
+        color: Colors.green,
+      ),
+      child: const Icon(
+        Icons.play_arrow,
+        size: 35,
+        color: Colors.black,
+      ),
+    );
+  }
+}
