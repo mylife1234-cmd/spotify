25958f5a818f8b90c108b9834cdf228d8eac4293
mylife1234-cmd <mdc011645996@gmail.com>
2022-03-23 01:50:23 +0700

    Update search screen ( Đã update màu chỉnh sửa các thứ thoe thiết kế)

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -136,6 +136,83 @@ class SearchPage extends StatelessWidget {
                         );
                       },
                     ),
+                    const Padding(
+                      padding: EdgeInsets.only(bottom: 15, top: 10),
+                      child: Text(
+                        "Browse All",
+                        style: TextStyle(
+                          fontSize: 17,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    GridView.builder(
+                      itemCount: listMusic1.length,
+                      gridDelegate:
+                      const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        childAspectRatio: 1.75,
+                        mainAxisSpacing: 15,
+                        crossAxisSpacing: 15,
+                      ),
+                      shrinkWrap: true,
+                      controller: ScrollController(keepScrollOffset: false),
+                      itemBuilder: (BuildContext context, int index) {
+                        final item1 = listMusic1[index];
+                        final colorValues =
+                        item1['colors']!.split(', ').map((e) {
+                          return int.parse(e);
+                        });
+
+                        return ClipRRect(
+                          child: Container(
+                            decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                begin: Alignment.topLeft,
+                                end: const Alignment(0.8, 0.0),
+                                colors:
+                                colorValues.map((e) => Color(e)).toList(),
+                              ),
+                              borderRadius: BorderRadius.circular(5),
+                            ),
+                            child: Stack(
+                              children: <Widget>[
+                                Positioned(
+                                  top: 15,
+                                  left: 15,
+                                  child: ConstrainedBox(
+                                    constraints: BoxConstraints(
+                                      maxWidth: size.width / 4.5,
+                                    ),
+                                    child: Text(
+                                      item1['text']!,
+                                      style: const TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 17,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                Positioned(
+                                  top: 30,
+                                  right: -20,
+                                  child: RotationTransition(
+                                    turns:
+                                    const AlwaysStoppedAnimation(25 / 360),
+                                    child: Image.asset(
+                                      item1['image']!,
+                                      height: 80,
+                                      width: 80,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    ),
                   ],
                 ),
               ),
@@ -151,36 +228,44 @@ final listMusic = [
   {
     'text': "K/DA",
     'image': "assets/images/home/kda.jpg",
-    "colors": '0xFFD4AF37, 0xFFCFB53B',
+    "colors": '0xFF9C2780, 0xFFE040FB',
   },
   {
     'text': "Bigcityboi",
     'image': "assets/images/home/big-city-boi.jpg",
-    "colors": '0xff031cd7, 0xFFCFB53B'
+    "colors": '0xff4caf50, 0xFF69F0AE'
   },
   {
     'text': "DNA",
     'image': "assets/images/home/dna.jpg",
-    "colors": '0xFFD4AF37, 0xFFCFB53B'
+    "colors": '0xFF2196F3, 0xFF448AFF'
   },
   {
     'text': "Latata",
     'image': "assets/images/home/latata.jpg",
-    "colors": '0xff031cd7, 0xFFCFB53B'
+    "colors": '0xfff44336, 0xFFff5252'
   },
+];
+final listMusic1 = [
   {
     'text': "Chilled",
     'image': "assets/images/home/chilled.jpg",
-    "colors": '0xFFD4AF37, 0xFFCFB53B'
+    "colors": '0xff8bca4a, 0xFFB2FF59'
   },
   {
     'text': "Ái Nộ",
     'image': "assets/images/home/ai-no.jpg",
-    "colors": '0xff031cd7, 0xFFCFB53B'
+    "colors": '0xff455a64, 0xff455a64'
   },
   {
     'text': "Relax",
     'image': "assets/images/home/album2.jpg",
-    "colors": '0xff031cd7, 0xFFCFB53B'
+    "colors": '0xFF9CCC65, 0xFF9CCC65'
   },
+  {
+    'text': "Latata",
+    'image': "assets/images/home/latata.jpg",
+    "colors": '0xFFBA68C8, 0xFFBA68C8'
+  },
+
 ];
