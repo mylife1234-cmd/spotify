b46ad3bc660fefd2362476dd6c6c25f780389aff
Duy <M.Duy2001@gmail.com>
2022-03-30 00:14:04 +0700

    Updated the sign-up page

--- lib/pages/signup.dart
+++ lib/pages/signup.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 import '../components/auth/input_field.dart';
@@ -37,78 +39,90 @@ class _SignUpPageState extends State<SignUpPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Create account',
-          style: TextStyle(fontWeight: FontWeight.w800),
-        ),
-        backgroundColor: Colors.black,
-        elevation: 0,
-        centerTitle: true,
-        leading: GestureDetector(
-          child: const BackButtonIcon(),
-          onTap: () {
-            if (_currentOption == 0) {
-              Navigator.maybePop(context);
-            } else {
-              _controller.text = results.values.elementAt(_currentOption - 1);
-              setState(() {
-                _currentOption--;
-              });
+    return GestureDetector(
+      onHorizontalDragEnd: Platform.isIOS
+          ? (details) {
+              if (details.primaryVelocity! > 0) pop();
             }
-          },
-        ),
-      ),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.only(left: 10, right: 10, top: 10),
-          child: Column(
-            children: [
-              InputField(
-                controller: _controller,
-                label: options[_currentOption]['label']!,
-                onChanged: (value) => setState(() {
-                  results[options[_currentOption]['name']!] = value;
-                }),
-                helperText: options[_currentOption]['helper'],
-              ),
-              const SizedBox(height: 50),
-              NextButton(
-                label: _currentOption < options.length - 1
-                    ? 'Next'
-                    : 'Create an account',
-                color: (results.length >= _currentOption + 1 &&
-                        results.values.elementAt(_currentOption).isNotEmpty)
-                    ? Colors.white
-                    : const Color(0xff4d4d4d),
-                onTap: !(results.length >= _currentOption + 1 &&
-                        results.values.elementAt(_currentOption).isNotEmpty)
-                    ? null
-                    : () {
-                        if (_currentOption < options.length - 1) {
-                          if (results.length - 1 >= _currentOption + 1 &&
-                              results.values
-                                  .elementAt(_currentOption + 1)
-                                  .isNotEmpty) {
-                            _controller.text =
-                                results.values.elementAt(_currentOption + 1);
-                          } else {
-                            _controller.text = '';
-                          }
+          : null,
+      child: WillPopScope(
+        onWillPop: () async => false,
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text(
+              'Create account',
+              style: TextStyle(fontWeight: FontWeight.w800),
+            ),
+            backgroundColor: Colors.black,
+            elevation: 0,
+            centerTitle: true,
+            leading: GestureDetector(
+              child: const BackButtonIcon(),
+              onTap: pop,
+            ),
+          ),
+          body: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 10, right: 10, top: 10),
+              child: Column(
+                children: [
+                  InputField(
+                    controller: _controller,
+                    label: options[_currentOption]['label']!,
+                    onChanged: (value) => setState(() {
+                      results[options[_currentOption]['name']!] = value;
+                    }),
+                    helperText: options[_currentOption]['helper'],
+                  ),
+                  const SizedBox(height: 50),
+                  NextButton(
+                    label: _currentOption < options.length - 1
+                        ? 'Next'
+                        : 'Create an account',
+                    color: (results.length >= _currentOption + 1 &&
+                            results.values.elementAt(_currentOption).isNotEmpty)
+                        ? Colors.white
+                        : const Color(0xff4d4d4d),
+                    onTap: !(results.length >= _currentOption + 1 &&
+                            results.values.elementAt(_currentOption).isNotEmpty)
+                        ? null
+                        : () {
+                            if (_currentOption < options.length - 1) {
+                              if (results.length - 1 >= _currentOption + 1 &&
+                                  results.values
+                                      .elementAt(_currentOption + 1)
+                                      .isNotEmpty) {
+                                _controller.text = results.values
+                                    .elementAt(_currentOption + 1);
+                              } else {
+                                _controller.text = '';
+                              }
 
-                          setState(() {
-                            _currentOption++;
-                          });
-                        } else {
-                          print(results);
-                        }
-                      },
-              )
-            ],
+                              setState(() {
+                                _currentOption++;
+                              });
+                            } else {
+                              print(results);
+                            }
+                          },
+                  )
+                ],
+              ),
+            ),
           ),
         ),
       ),
     );
+  }
+
+  pop() {
+    if (_currentOption == 0) {
+      Navigator.pop(context);
+    } else {
+      _controller.text = results.values.elementAt(_currentOption - 1);
+      setState(() {
+        _currentOption--;
+      });
+    }
   }
 }
