a692fa90131be474fd3da3a576bdef30ad3364de
Lppt12345 <aibiettoi19@gmail.com>
2022-04-02 05:24:23 +0700

    Add playing_song file

---
+++ lib/components/queue/playing_song.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../../models/song.dart';
+import '../../providers/music_provider.dart';
+
+class PlayingSongTile extends StatelessWidget {
+  const PlayingSongTile({Key? key, required this.song}) : super(key: key);
+  final Song song;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 12),
+      width: double.infinity,
+      color: Colors.black,
+      child: ListTile(
+        title: Text(
+          song.name,
+          style: const TextStyle(
+            fontWeight: FontWeight.w700 ,
+            color: Colors.green,
+          ),
+          overflow: TextOverflow.ellipsis,
+          maxLines: 1,
+        ),
+        subtitle: Text(song.description,
+            overflow: TextOverflow.ellipsis,
+            maxLines: 1,
+        ),
+        leading: ClipRRect(
+          borderRadius: BorderRadius.circular(5),
+          child: Image.asset(song.coverUrl),
+        ),
+        contentPadding: EdgeInsets.zero,
+        dense: true,
+        visualDensity: VisualDensity.standard,
+        horizontalTitleGap: 12,
+        onTap: () {
+          context.read<MusicProvider>().playNewSong(song);
+        },
+      ),
+    );
+  }
+}
