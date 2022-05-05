35a179d91d062be853a7197351fa9d13dd5b4e07
Lppt12345 <aibiettoi19@gmail.com>
2022-05-05 13:02:04 +0700

    delete playlist

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -22,7 +22,6 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     super.initState();
-
     getIt.registerSingleton<BuildContext>(context, instanceName: 'homeContext');
   }
 
