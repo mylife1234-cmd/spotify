ba0258b7d2a4acb3811d05e14d9cb53e9b266495
Duy <M.Duy2001@gmail.com>
2022-05-11 02:50:22 +0700

    Updated the start screen

--- lib/pages/start.dart
+++ lib/pages/start.dart
@@ -36,118 +36,126 @@ class StartPage extends StatelessWidget {
           ),
           Center(
             child: SafeArea(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 20),
-                    child: Image.asset(
-                      'assets/images/white-logo.png',
-                      width: 60,
-                    ),
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.symmetric(vertical: 10),
-                    child: Text(
-                      'Millions of Songs.',
-                      style: TextStyle(
-                        fontSize: 26,
-                        fontWeight: FontWeight.w800,
+              child: SingleChildScrollView(
+                physics: const ClampingScrollPhysics(),
+                child: SizedBox(
+                  height: MediaQuery.of(context).size.height -
+                      MediaQuery.of(context).padding.top -
+                      MediaQuery.of(context).padding.bottom,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 20),
+                        child: Image.asset(
+                          'assets/images/white-logo.png',
+                          width: 60,
+                        ),
                       ),
-                    ),
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.only(bottom: 30),
-                    child: Text(
-                      'Free on Spotify.',
-                      style: TextStyle(
-                        fontSize: 26,
-                        fontWeight: FontWeight.w800,
+                      const Padding(
+                        padding: EdgeInsets.symmetric(vertical: 10),
+                        child: Text(
+                          'Millions of Songs.',
+                          style: TextStyle(
+                            fontSize: 26,
+                            fontWeight: FontWeight.w800,
+                          ),
+                        ),
                       ),
-                    ),
-                  ),
-                  LoginButton(
-                    text: 'Sign up free',
-                    color: const Color(0xff1ed760),
-                    textColor: Colors.black,
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) {
-                          return const SignUpPage();
-                        }),
-                      );
-                    },
-                  ),
-                  LoginButton(
-                    text: 'Continue with Google',
-                    color: Colors.black,
-                    textColor: Colors.white,
-                    borderColor: Colors.white,
-                    leadingIcon: Positioned(
-                      top: 13,
-                      left: 13,
-                      child: Image.asset(
-                        'assets/images/brands/google.png',
-                        width: 26,
+                      const Padding(
+                        padding: EdgeInsets.only(bottom: 30),
+                        child: Text(
+                          'Free on Spotify.',
+                          style: TextStyle(
+                            fontSize: 26,
+                            fontWeight: FontWeight.w800,
+                          ),
+                        ),
                       ),
-                    ),
-                    onTap: () async {
-                      try {
-                        final googleUser = await GoogleSignIn().signIn();
+                      LoginButton(
+                        text: 'Sign up free',
+                        color: const Color(0xff1ed760),
+                        textColor: Colors.black,
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) {
+                              return const SignUpPage();
+                            }),
+                          );
+                        },
+                      ),
+                      LoginButton(
+                        text: 'Continue with Google',
+                        color: Colors.black,
+                        textColor: Colors.white,
+                        borderColor: Colors.white,
+                        leadingIcon: Positioned(
+                          top: 13,
+                          left: 13,
+                          child: Image.asset(
+                            'assets/images/brands/google.png',
+                            width: 26,
+                          ),
+                        ),
+                        onTap: () async {
+                          try {
+                            final googleUser = await GoogleSignIn().signIn();
 
-                        final googleAuth = await googleUser?.authentication;
+                            final googleAuth = await googleUser?.authentication;
 
-                        final credential = GoogleAuthProvider.credential(
-                          accessToken: googleAuth?.accessToken,
-                          idToken: googleAuth?.idToken,
-                        );
+                            final credential = GoogleAuthProvider.credential(
+                              accessToken: googleAuth?.accessToken,
+                              idToken: googleAuth?.idToken,
+                            );
 
-                        final userCredential = await FirebaseAuth.instance
-                            .signInWithCredential(credential);
+                            final userCredential = await FirebaseAuth.instance
+                                .signInWithCredential(credential);
 
-                        initUser(userCredential);
-                      } catch (e) {
-                        debugPrint(e.toString());
-                      }
-                    },
+                            initUser(userCredential);
+                          } catch (e) {
+                            debugPrint(e.toString());
+                          }
+                        },
+                      ),
+                      LoginButton(
+                        text: 'Reset password',
+                        color: Colors.black,
+                        textColor: Colors.white,
+                        borderColor: Colors.white,
+                        margin: const EdgeInsets.only(bottom: 5),
+                        leadingIcon: const Positioned(
+                          top: 13,
+                          left: 14,
+                          child: Icon(Icons.password),
+                        ),
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) {
+                              return const ResetPasswordPage();
+                            }),
+                          );
+                        },
+                      ),
+                      LoginButton(
+                        text: 'Log in',
+                        color: Colors.black,
+                        textColor: Colors.white,
+                        borderColor: Colors.black,
+                        margin: const EdgeInsets.only(bottom: 20),
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) {
+                              return const LoginPage();
+                            }),
+                          );
+                        },
+                      ),
+                    ],
                   ),
-                  LoginButton(
-                    text: 'Reset password',
-                    color: Colors.black,
-                    textColor: Colors.white,
-                    borderColor: Colors.white,
-                    margin: const EdgeInsets.only(bottom: 5),
-                    leadingIcon: const Positioned(
-                      top: 13,
-                      left: 14,
-                      child: Icon(Icons.password),
-                    ),
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) {
-                          return const ResetPasswordPage();
-                        }),
-                      );
-                    },
-                  ),
-                  LoginButton(
-                    text: 'Log in',
-                    color: Colors.black,
-                    textColor: Colors.white,
-                    borderColor: Colors.black,
-                    margin: const EdgeInsets.only(bottom: 20),
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) {
-                          return const LoginPage();
-                        }),
-                      );
-                    },
-                  ),
-                ],
+                ),
               ),
             ),
           ),
