b774dcfaa86782d5777086eb9f86df896550dff6
Duy <M.Duy2001@gmail.com>
2022-05-12 18:45:25 +0700

    Updated the player

--- lib/pages/music/player.dart
+++ lib/pages/music/player.dart
@@ -26,65 +26,69 @@ class MusicPlayer extends StatelessWidget {
 
     final image = getImageFromUrl(song!.coverImageUrl);
 
+    final topPadding = MediaQuery.of(context).viewPadding.top;
+
     return Scaffold(
       backgroundColor: color,
-      body: MediaQuery.removePadding(
-        context: context,
-        removeTop: size.height < 700,
-        child: ListView(
+      body: Container(
+        margin: EdgeInsets.only(top: topPadding),
+        child: SingleChildScrollView(
           physics: const ClampingScrollPhysics(),
-          children: [
-            Padding(
-              padding: EdgeInsets.only(top: padding),
-              child: PlayerHeader(
-                onDismissed: () {
-                  Navigator.maybePop(context);
-                },
-                song: song,
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(),
+                child: PlayerHeader(
+                  onDismissed: () => Navigator.maybePop(context),
+                  song: song,
+                ),
               ),
-            ),
-            Padding(
-              padding:
-                  EdgeInsets.symmetric(horizontal: 24, vertical: padding * 0.8),
-              child: Image(image: image),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 24),
-              child: InfoSection(
-                name: song.name,
-                description: song.description,
-                song: song,
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 24,
+                  vertical: padding * 0.8,
+                ),
+                child: Image(image: image),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                child: InfoSection(
+                  name: song.name,
+                  description: song.description,
+                  song: song,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(
                   left: 18,
                   right: 18,
                   top: padding * 0.6,
-                  bottom: padding * 0.2),
-              child: const MusicSlider(),
-            ),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 26),
-              child: ControllerSection(),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                left: 24,
-                right: 22,
-                bottom: 12,
-                top: 16,
+                  bottom: padding * 0.2,
+                ),
+                child: const MusicSlider(),
               ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  ShareButton(song: song, size: 22),
-                  QueueButton(song: song, size: 26),
-                ],
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 26),
+                child: ControllerSection(),
               ),
-            ),
-            const LyricSection(),
-          ],
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 24,
+                  right: 22,
+                  bottom: 12,
+                  top: 16,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ShareButton(song: song, size: 22),
+                    QueueButton(song: song, size: 26),
+                  ],
+                ),
+              ),
+              const LyricSection(),
+            ],
+          ),
         ),
       ),
     );
